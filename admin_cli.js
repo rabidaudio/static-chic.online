6d1bc89a3d940376aaa0326dc5376ed2dffc8a83
Julien <julien@rabidaudio.com>
2026-03-08 23:49:12 -0400

    refactor cli

--- admin_cli.js
+++ admin_cli.js
@@ -5,49 +5,70 @@ const app = require('./app')
 
 function main () {
   yargs(hideBin(process.argv))
+    .scriptName('manage')
     .command('get_user <username>', 'check the status of a user', (yargs) => {
       return yargs.positional('username', { describe: 'the unique username of the user' })
-    }, async (argv) => {
-      const user = await app.getUser(argv.username)
+    }, async ({ username }) => {
+      const user = await app.getUser(username)
 
       if (user) {
         console.log(user)
       } else {
-        console.error(`No user found for username "${argv.username}"`)
+        console.error(`No user found for username "${username}"`)
       }
     })
     .command('add_user <username> [details]', 'create a new user', (yargs) => {
       return yargs
         .positional('username', { describe: 'the unique username of the user' })
-        .alias('n', 'name').describe('n', 'The display name of the user')
+        .option('name', {
+          alias: 'n',
+          describe: 'The display name of the user'
+        })
         .demandOption(['n'])
-    }, async (argv) => {
+    }, async ({ username, name }) => {
       // TODO: auth tokens?
 
-      const user = await app.createUser({ userId: argv.username, name: argv.name })
+      const user = await app.createUser({ userId: username, name })
       console.log(user)
     })
     .command('list_sites <username>', 'show all the sites managed by the user', (yargs) => {
       return yargs
         .positional('username', { describe: 'the username of the user' })
-    }, async (argv) => {
+    }, async ({ username }) => {
       // TODO: auth tokens?
 
-      const sites = await app.listSitesForUser(argv.username)
+      const sites = await app.listSitesForUser(username)
       console.log(sites)
     })
-
-  // exports. = async (userId) => {
 
     .command('add_site', 'create a new site', (yargs) => {
       return yargs
         .example('add_site -o [username] -n [name]')
-        .alias('o', 'owner').describe('o', 'The username of the site owner')
-        .alias('n', 'name').describe('n', 'The name of the site')
+        .option('owner', {
+          alias: 'o',
+          describe: 'The username of the site owner'
+        })
+        .option('name', {
+          alias: 'n',
+          describe: 'The name of the site'
+        })
         .demandOption(['o', 'n'])
-    }, async (argv) => {
-      const site = await app.createSite({ name: argv.name, userId: argv.owner })
+    }, async ({ name, owner }) => {
+      const site = await app.createSite({ name, userId: owner })
       console.log(site)
+    })
+
+    .command('list_deployments [site]', 'list all the deployments for a site', (yargs) => {
+      return yargs
+        .positional('site', { describe: 'the siteId' })
+    }, async ({ site }) => {
+      const siteData = await app.getSite(site)
+      const deployments = await app.listDeployments(site)
+      console.log(deployments.map(d => {
+        if (siteData.currentDeployment === d.deploymentId) return { ...d, current: true }
+        else return d
+      }))
+      // TODO: show in table form instead
     })
 
     .command('deploy [path]', 'create a deployment to the given site', (yargs) => {
@@ -69,16 +90,24 @@ function main () {
           describe: "Also promote the deployment.\nA deployment isn't automatically promoted to live by default",
           type: 'boolean'
         })
+        .option('wait', {
+          alias: 'w',
+          describe: 'wait for the invalidation to complete (if also promoting)',
+          type: 'boolean'
+        })
         .demandOption(['site'])
-    }, async (argv) => {
-      console.log("zipping and uploading...")
-      const contentTarball = await app.createTarball(argv.path) // TODO: exclude
-      const deployment = await app.createDeployment({ siteId: argv.site, contentTarball })
+    }, async ({ path, site, exclude, promote, wait }) => {
+      console.log('zipping and uploading...')
+      const contentTarball = await app.createTarball(path) // TODO: exclude
+      const deployment = await app.createDeployment({ siteId: site, contentTarball })
       console.log(deployment)
-      if (argv.promote) {
-        console.log("promoting...")
-        const site = await app.promoteDeployment(deployment)
-        console.log(site)
+      if (promote) {
+        console.log('promoting...')
+        const deployedSite = await app.promoteDeployment(deployment)
+        console.log(deployedSite)
+        if (wait) {
+          // TODO: wait until invalidation complete
+        }
       }
     })
 
@@ -91,18 +120,16 @@ function main () {
         .positional('deploymentId')
         .option('wait', {
           alias: 'w',
-          describe: "wait for the invalidation to complete",
-          type: "boolean",
+          describe: 'wait for the invalidation to complete',
+          type: 'boolean'
         })
-    }, async (argv) => {
-        console.log(argv)
-        const { siteId, deploymentId } = argv
-        console.log("promoting...")
-        const site = await app.promoteDeployment({ siteId, deploymentId })
-        console.log(site)
-        if (argv.wait) {
-          // TODO: wait until invalidation complete
-        }
+    }, async ({ siteId, deploymentId, wait }) => {
+      console.log('promoting...')
+      const site = await app.promoteDeployment({ siteId, deploymentId })
+      console.log(site)
+      if (wait) {
+        // TODO: wait until invalidation complete
+      }
     })
 
     .help('h')
